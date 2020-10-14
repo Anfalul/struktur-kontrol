010ccf17e8b66aa6327de47808e7f3591ebae31c
Anfalul <72851809+Anfalul@users.noreply.github.com>
2020-10-14 15:00:41 +0700

    Add files via upload

---
+++ StrukturKontrol.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package struktur.kontrol;
+
+/**
+ *
+ * @author ASUS
+ */
+public class StrukturKontrol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //if else if statement
+        
+        int umur=30 ;
+        
+        System.out.println("awal dari program");
+         
+        if ( umur==25){
+             System.out.println("maka saya akan menikah");
+             
+        }else if ( umur==18){ 
+            System.out.println("saya akan kuliah ");
+           
+        }else 
+            System.out.println("saya akan kerja");
+        
+        //akhir dari program
+         System.out.println("akihr dari program");
+        
+        
+    }
+    
+}
